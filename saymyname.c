b48d6f04044650edf9dfce568d0cfb2145c7770e
gtstrike <124995420+gtstrike@users.noreply.github.com>
2023-02-17 21:41:04 +0700

    simple prompt is done
    ask name
    if correct it will respond

--- saymyname.c
+++ saymyname.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 #include<cs50.h>
+#include<string.h>
 int main(void)
 {
-    string name =get_string("say my name!\n");
-    printf("you're goddam right\n");
+    //char name[50] = "heisenberg";
+    char name2[50] = "heisenberg";
+    char name[50];
+    printf("say my name!\n");
+    //fgets(name, sizeof(name), stdin);
+    // printf("%s", name);
+    scanf("%s", name);
+    if (strcmp(name, name2) == 0)
+    {
+        printf("you're goddam right\n");
+    }
+    return 0;
 }
